552c87b260cd9fe5dd31aa28cecf901bd649c1ac
ayussh176 <ayushmalik852@gmail.com>
2025-07-22 20:15:46 +0530

    coading platorm udated

--- src/components/CodingPlatforms.tsx
+++ src/components/CodingPlatforms.tsx
@@ -26,7 +26,7 @@ const CodingPlatforms = () => {
       <h2 className="text-3xl font-bold text-purple-600 mb-10">Coding Platforms</h2>
       <div className="h-1 w-20 bg-purple-600 mx-auto mb-6"></div>
       <div className="container mx-auto px-4 flex flex-wrap justify-center gap-8">
-
+      
         {platforms.map((platform) => (
           <a
             href={platform.link}
